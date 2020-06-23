cd654f0de4c1fbcd17e606c6f2d1833426ff87af
vegardinho <landsverk.vegard@gmail.com>
2020-06-23 21:41:14 +0200

    Combined xml and ics check. Now deletes events that contain avlys, and include latest info in event summary

--- post_event.py
+++ post_event.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import xml.etree.ElementTree as ET
 from datetime import date,timedelta,datetime
 from dateutil.relativedelta import relativedelta
 import pickle
@@ -10,15 +11,17 @@ from ics import Calendar
 import requests
 from googleapiclient.errors import HttpError
 from my_logger import MyLogger
+import arrow
 
 MONTHS_FWD = 12
 MONTHS_BACK = 2
 TODAY = datetime.today()
 START_DATE = TODAY - relativedelta(months=MONTHS_BACK)
 END_DATE = TODAY + relativedelta(months=MONTHS_FWD)
+NM_SPS = {"ss": "urn:schemas-microsoft-com:office:spreadsheet"}
 
-log_obj = MyLogger("mlog", cre_f_ha=True, root="logs/", sys_ha="INFO", f_ha="DEBUG", 
-    logger_level="DEBUG")
+log_obj = MyLogger("mlog", cre_f_ha=True, cre_sys_h=True, root="logs/", sys_ha="INFO", f_ha="DEBUG", 
+    logger_level="DEBUG", o_write_all=True)
 log_obj.add_handler(level="INFO", filename="info.log")
 log_obj.add_handler(level="WARNING", filename="err.log")
 mlog = log_obj.retrieve_logger()
@@ -36,6 +39,7 @@ def main():
 
 def post_event(service, event, action="upload"):
     mlog.info("Posting event {} ({}) with action: {}".format(event["summary"], event["start"], action))
+
     try:
         if action == "upload":
             event = service.events().insert(calendarId='primary', body=event).execute()
@@ -123,61 +127,136 @@ def setup():
 # Get .ics file from site (with events within specified dates), and make dictionary with
 # formatting matched to that of google events with relevant info for each event, and return 
 # list. ID is changed to fit limitations in google calendar id pattern.
-def get_events():
+def get_events_ics():
+    mlog.info("Downloading ICS-events")
+
     url = 'https://eventor.orientering.no/Events/ExportICalendarEvents?startDate={}&endDate={}&organisations=5%2C19&classifications=International%2CChampionship%2CNational%2CRegional%2CLocal'\
         .format(START_DATE, END_DATE)
     response = requests.get(url)
     response.encoding = 'utf-8'
     c = Calendar(response.text)
 
-    eventorEv = list(c.timeline)
+    ics_evs = list(c.timeline)
+    return ics_evs
+
+
+def get_events_xml():
+    mlog.info("Downloading XML-events")
+
+    url = 'https://eventor.orientering.no/Events/ExportToExcel?startDate={}&endDate={}&organisations=5%2C19&classifications=International%2CChampionship%2CNational%2CRegional%2CLocal'\
+        .format(START_DATE, END_DATE)
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    mlog.debug(response.text)
+
+
+    root = ET.fromstring(response.text)
+    # root = tree.getroot()
+    table = root.find("./ss:Worksheet[@ss:Name=\"Konkurranser\"]/ss:Table", NM_SPS)
+    # First row contains info on structure
+    xml_evs = table.findall("ss:Row", NM_SPS)[1:]
+
+    return xml_evs
+
+def get_events():
+    START_IND = 0
+    END_IND = 1
+    NAME_IN = 2
+    CLUB_IN = 3
+    DISTRICT_IN = 4
+    DISTANCE_IN = 7
+    INFO_IN = 11
+
+    xml_evs = get_events_xml()
+    ics_evs = get_events_ics()
+
+    if (len(xml_evs) != len(ics_evs)):
+        mlog.critical("XML and ICS do not coincide. Aborting.")
+        exit()
+
+    mlog.info("Making event structs")
     events = []
 
-    for event in eventorEv:
-        summary = event.name.split(", ")
+    for i in range(len(xml_evs)):
+        ics_ev = ics_evs[i]
+        xml_ev = xml_evs[i]
 
-        time = get_time_format(event.begin, event.end)
+        #Skip events that are cancelled
+        xml_info = xml_ev_info(xml_ev, START_IND, END_IND)
+        info = xml_info[INFO_IN]
+        if (info and ("avlys" in info.lower()) or "avlys" in xml_info[NAME_IN].lower()):
+            continue
 
-        try:
-            info = {
-                'summary': '{} ({})'.format("".join(summary[:-1]), summary[-1]),
-                "source": {
-                    'title': 'Eventor-arrangement',
-                    'url': event.url
-                    },
-                'id': event.uid.split("@")[0].lower().replace('_', ''),
-                'description': 'Se {} for mer informasjon'.format(event.url),
-                "start": time[0],
-                "end": time[1]
-                }
-            if event.geo:
-                info.update({"location": "{}, {}".format(event.geo[0], event.geo[1])})
-        except TypeError as e:
-            mlog.warning("%s" % e)
+        name_list = ics_ev.name.split(", ")
+        summary = '{} ({})'.format("".join(name_list[:-1]), name_list[-1])
+        e_id = ics_ev.uid.split("@")[0].lower().replace('_', '')
+        time = get_time_format(xml_info[START_IND], xml_info[END_IND])
 
-        events.append(info)
+        e_pkt = make_packet(summary, ics_ev.url, e_id, time, ics_ev.geo, xml_info[INFO_IN])
+        events.append(e_pkt)
 
     mlog.debug(events)
     return events
 
+def make_packet(summary, e_url, e_id, time, e_geo, e_info):
+    try:
+        info = {
+            'summary': summary,
+            "source": {
+                'title': 'Eventor-arrangement',
+                'url': e_url
+                },
+            'id': e_id,
+            'description': 'Se <a href="{0}" target="_blank">{0}</a> for mer informasjon'.format(e_url),
+            "start": time[0],
+            "end": time[1]
+            }
+        if e_geo:
+            info.update({"location": "{}, {}".format(e_geo[0], e_geo[1])})
+        if e_info:
+            info['description'] = "{}\n\n{}".format(e_info, info['description'])
+    except TypeError as e:
+        mlog.warning("%s" % e)  
+
+    return info
+
+def xml_ev_info(xml_ev, START_IND, END_IND):
+    tree = xml_ev
+    info = []
+
+    for node in tree:
+        info.append(node.findtext("./ss:Data", namespaces=NM_SPS))
+
+    info[START_IND] = arrow.get(info[START_IND])
+    if info[END_IND] == None:
+        if info[START_IND].timetuple().tm_hour == 0:
+            add_delta = relativedelta(days=1)
+        else:
+            add_delta = relativedelta(hours=4)
+        info[END_IND] = info[START_IND] + add_delta
+    else:
+        info[END_IND] = arrow.get(info[END_IND])
+    info[END_IND] = info[END_IND].replace(tzinfo='Europe/Oslo')
+    info[START_IND] = info[START_IND].replace(tzinfo='Europe/Oslo')
+    return info
+
 # Change time format to fit google event specifications. Change to date-format (as opposed
 # to datetime), if events start and end at midnight (00/24)
 def get_time_format(start, end):
+    s_date = start
+    e_date = end
 
-    s_date = start.to('Europe/Oslo')
-    e_date = end.to('Europe/Oslo')
-
-    if (s_date.timetuple().tm_hour == s_date.timetuple().tm_hour == 0):
+    if (s_date.timetuple().tm_hour == e_date.timetuple().tm_hour == 0):
         start = {'date': s_date.format("YYYY-MM-DD")}
         end = {'date': e_date.format("YYYY-MM-DD")}
     else:
         start = {
             'dateTime': s_date.format("YYYY-MM-DDTHH:mm:ssZZ"), 
-            'timeZone': 'Etc/UTC'
+            'timeZone': 'Europe/Oslo'
             }
         end = {
             'dateTime': e_date.format("YYYY-MM-DDTHH:mm:ssZZ"), 
-            'timeZone': 'Etc/UTC'
+            'timeZone': 'Europe/Oslo'
             }
     return [start, end]
 
