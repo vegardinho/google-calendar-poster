2eae7ef7831702eb7e04856dc2e57ed45d78d7c3
vegardinho <landsverk.vegard@gmail.com>
2021-01-19 20:04:34 +0100

    Uses wget to check internet connection; launches check_last_run.py if no connection

--- run_post_script.sh
+++ run_post_script.sh
@@ -4,6 +4,18 @@ cd /Users/vegardlandsverk/Documents/Progging/google_calendar
 source bin/activate
 python3 post_event.py
 ret_val=$?
-deactivate
 
-exit $ret_val
+if [ $ret_val -eq 0 ]
+then
+	exit_val=0
+else
+	/usr/local/bin/wget -q --spider http://google.com
+	if [[ $? -eq 0 ]]
+	then
+		python3 check_last_run.py
+	fi
+	exit_val=-1
+fi
+
+deactivate
+exit $exit_val
