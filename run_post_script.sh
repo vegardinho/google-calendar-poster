a6002c7d3f409bdf310509b804d93a57b135c432
vegardinho <landsverk.vegard@gmail.com>
2021-06-25 16:48:49 +0200

    Cleanup old logs after each run

--- run_post_script.sh
+++ run_post_script.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+# Run script and enter virtual environment
 cd /Users/vegardlandsverk/Documents/Progging/google_calendar
 source bin/activate
 python3 post_event.py
 ret_val=$?
 
+
+# Run check of last successfull run if failed run and internet connection
 if [ $ret_val -eq 0 ]
 then
 	exit_val=0
@@ -16,6 +19,9 @@ else
 	fi
 	exit_val=-1
 fi
+
+# Move all old logs into archive folder
+mv ./logs/*.log.* ./logs/archive/
 
 deactivate
 exit $exit_val
