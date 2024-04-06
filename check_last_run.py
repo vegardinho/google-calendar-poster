334c675226ac71b0d0ea9eeda6834eccc5f5889c
vegardinho <landsverk.vegard@gmail.com>
2024-04-06 16:43:18 +0200

    Fix send_mail and remove_old_files implementations.

--- check_last_run.py
+++ check_last_run.py
@@ -1,23 +1,27 @@
-import arrow
-import send_email
 import os.path
-import post_event as pe
 import traceback
-from remove_old_files import remove_old_files
 from datetime import timedelta
 
-FILE_SEC = timedelta(days = .5).total_seconds()
-EMAIL_SEC = timedelta(days = 1).total_seconds()
+from send_email import send_email
+from remove_old_files import remove_old_files
+
+import post_event as pe
+
+FILE_SEC = timedelta(days=.5).total_seconds()
+EMAIL_SEC = timedelta(days=1).total_seconds()
 EMAIL_FILE = "./email.out"
-SEC_PER_DAY = timedelta(days = 1).total_seconds()
+SEC_PER_DAY = timedelta(days=1).total_seconds()
 
 NOW = pe.TODAY.timestamp
-LOGS_EXP_SEC = timedelta(days = 30).total_seconds()
+LOGS_EXP_SEC = timedelta(days=30).total_seconds()
 
 
 def main():
     check_if_run()
-    remove_old_files("./logs/archive/", LOGS_EXP_SEC)
+    try:
+        remove_old_files("./logs/archive/", LOGS_EXP_SEC)
+    except Exception as err:
+        pe.log.warning(err)
 
 
 def check_if_run():
@@ -25,33 +29,32 @@ def check_if_run():
     email_tmstmp = 0
     try:
         file_tmstmp = os.path.getmtime(pe.SUCCESS_FILE)
-    except Exception as e:
+    except Exception as err:
         pe.log.warning("Could not find success-file. Setting timestamp to zero.")
 
     try:
         email_tmstmp = os.path.getmtime(EMAIL_FILE)
-    except Exception as e:
+    except Exception as err:
         pe.log.warning("Could not find email-file. Setting timestamp to zero.")
 
     try:
         if ((NOW - file_tmstmp) > FILE_SEC and (NOW - email_tmstmp > EMAIL_SEC)):
             pe.log.warning("More than a week since last successfull run. Sending email!")
 
-            text = """Det er {:.1f} dag(er) siden en vellykket kjøring av Google
+            text = """Det er {:.1f} dag(er) siden en vellykket kjøring av Google \
                         Calendar-skriptet til skienok.no. Sjekk den derre loggen!""".format(
                 ((NOW - file_tmstmp) / SEC_PER_DAY))
             sub = "Hilsen fra Mons"
 
-            send_email.send_email("landsverk.vegard@gmail.com", "webansvarlig@skienok.no",
-                                  "system", "Gmail, privat", sub, text)
+            send_email("webansvarlig@skienok.no", sub, text)
             pe.log.warning("Marking email as sent")
             with open(EMAIL_FILE, "w") as f:
                 f.write("1")
         else:
-            pe.log.info("Last successfull run within chosen timeframe")
-    except Exception as e:
+            pe.log.info("Last successfully run within chosen timeframe")
+    except Exception as err:
         print(traceback.format_exc())
-        pe.log.error(e)
+        pe.log.error(err)
 
 
 if __name__ == "__main__":
@@ -61,3 +64,5 @@ if __name__ == "__main__":
         pe.log.info("CHECK END")
     except Exception as e:
         pe.log.error(e)
+
+# TODO: kombiner med email_errors i python-tools
